d138b4e8fefb28a68d14816787668ef2f2ef5283
printfn <printfn@users.noreply.github.com>
2025-03-26 03:12:09 +0000

    Activate furnaces with unburnable items

--- src/main/java/io/github/printfn/immersivefurnaces/mixins/FurnaceInvoker.java
+++ src/main/java/io/github/printfn/immersivefurnaces/mixins/FurnaceInvoker.java
@@ -1,11 +1,18 @@
 package io.github.printfn.immersivefurnaces.mixins;
 
+import javax.annotation.Nullable;
+
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.gen.Accessor;
 import org.spongepowered.asm.mixin.gen.Invoker;
 
 import net.minecraft.core.NonNullList;
+import net.minecraft.core.RegistryAccess;
 import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.crafting.AbstractCookingRecipe;
+import net.minecraft.world.item.crafting.RecipeHolder;
+import net.minecraft.world.item.crafting.RecipeManager;
+import net.minecraft.world.item.crafting.SingleRecipeInput;
 import net.minecraft.world.level.block.entity.AbstractFurnaceBlockEntity;
 
 @Mixin(AbstractFurnaceBlockEntity.class)
@@ -17,6 +24,12 @@ public interface FurnaceInvoker {
     void setLitTime(int litTime);
     @Accessor
     void setLitDuration(int litDuration);
+
+    @Accessor
+    RecipeManager.CachedCheck<SingleRecipeInput, ? extends AbstractCookingRecipe> getQuickCheck();
+
+    @Invoker
+    boolean invokeCanBurn(RegistryAccess registryAccess, @Nullable RecipeHolder<?> recipe, NonNullList<ItemStack> inventory, int maxStackSize, AbstractFurnaceBlockEntity furnace);
 
     @Invoker
     boolean invokeIsLit();
--- src/main/java/io/github/printfn/immersivefurnaces/mixins/FurnaceMixin.java
+++ src/main/java/io/github/printfn/immersivefurnaces/mixins/FurnaceMixin.java
@@ -6,6 +6,7 @@ import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 import net.minecraft.core.BlockPos;
+import net.minecraft.world.item.crafting.SingleRecipeInput;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.AbstractFurnaceBlock;
 import net.minecraft.world.level.block.entity.AbstractFurnaceBlockEntity;
@@ -24,22 +25,29 @@ public abstract class FurnaceMixin extends BaseContainerBlockEntity {
         var f = (FurnaceInvoker)blockEntity;
         var recipeInput = f.getItems().get(0);
         var fuel = f.getItems().get(1);
-        if (!f.invokeIsLit() && !fuel.isEmpty() && recipeInput.isEmpty()) {
-            var burnDuration = f.invokeGetBurnDuration(fuel);
-            f.setLitTime(burnDuration);
-            f.setLitDuration(burnDuration);
-            if (f.invokeIsLit()) {
-                if (fuel.hasCraftingRemainingItem()) {
-                    // e.g. using a lava bucket will leave behind an empty bucket
-                    f.getItems().set(1, fuel.getCraftingRemainingItem());
-                } else {
-                    fuel.shrink(1);
-                }
-                blockEntity.setChanged();
-                state = state.setValue(AbstractFurnaceBlock.LIT, Boolean.valueOf(true));
-                level.setBlock(pos, state, 3);
-                setChanged(level, pos, state);
+        if (f.invokeIsLit() || fuel.isEmpty()) {
+            return;
+        }
+        if (!recipeInput.isEmpty()) {
+            var recipeHolder = f.getQuickCheck().getRecipeFor(new SingleRecipeInput(recipeInput), level).orElse(null);
+            if (f.invokeCanBurn(level.registryAccess(), recipeHolder, f.getItems(), blockEntity.getMaxStackSize(), blockEntity)) {
+                return;
             }
+        }
+        var burnDuration = f.invokeGetBurnDuration(fuel);
+        f.setLitTime(burnDuration);
+        f.setLitDuration(burnDuration);
+        if (f.invokeIsLit()) {
+            if (fuel.hasCraftingRemainingItem()) {
+                // e.g. using a lava bucket will leave behind an empty bucket
+                f.getItems().set(1, fuel.getCraftingRemainingItem());
+            } else {
+                fuel.shrink(1);
+            }
+            blockEntity.setChanged();
+            state = state.setValue(AbstractFurnaceBlock.LIT, Boolean.valueOf(true));
+            level.setBlock(pos, state, 3);
+            setChanged(level, pos, state);
         }
     }
 }
